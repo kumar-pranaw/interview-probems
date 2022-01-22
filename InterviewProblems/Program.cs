af92645dcf1f871ea8cc47e528c836719d592ed5
Pranav kumar <sanupranav@outlook.com>
2022-01-22 21:41:20 +0530

    feat: Program For String Reverse

--- InterviewProblems/Program.cs
+++ InterviewProblems/Program.cs
@@ -1,9 +1,14 @@
-﻿namespace InterviewProblems
+﻿using System;
+
+namespace InterviewProblems
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            ReverseString reverseString = new ReverseString();
+            Console.WriteLine(reverseString.ReverseTheString("Pranav Kumar"));
+            Console.ReadKey();
         }
     }
 }
---
+++ InterviewProblems/ReverseString.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace InterviewProblems
+{
+    internal class ReverseString
+    {
+        public string ReverseTheString(string value)
+        {
+            string reversedString = String.Empty;
+            if (value != null)
+            {
+                var splittedText = value.ToCharArray();
+                for (int i = splittedText.Length - 1; i > - 1; i--)
+                {
+                    reversedString += splittedText[i];
+                }
+            }
+
+            return reversedString;
+        }
+    }
+}
