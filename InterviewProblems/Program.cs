11293330fd56ebf3308e6a3b04aeda18e26bd6a7
Pranav kumar <sanupranav@outlook.com>
2022-01-23 19:22:09 +0530

    feat: Static constructor program

--- InterviewProblems/Program.cs
+++ InterviewProblems/Program.cs
@@ -4,11 +4,16 @@ namespace InterviewProblems
 {
     internal class Program
     {
+        static Program()
+        {
+
+        }
         static void Main(string[] args)
         {
             ReverseString reverseString = new ReverseString();
-            Console.WriteLine(reverseString.ReverseTheString("Pranav Kumar"));
+            Console.WriteLine(reverseString.ReverseTheString("Pranav Kumar Verma"));
             Console.ReadKey();
         }
     }
+
 }
