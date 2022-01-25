6b3c671c63dea92962639490f5954fd98b40c296
Pranav kumar <sanupranav@outlook.com>
2022-01-25 09:25:38 +0530

    feat: Program for reversing string

--- InterviewProblems/Program.cs
+++ InterviewProblems/Program.cs
@@ -4,14 +4,11 @@ namespace InterviewProblems
 {
     internal class Program
     {
-        static Program()
-        {
-
-        }
         static void Main(string[] args)
         {
             ReverseString reverseString = new ReverseString();
             Console.WriteLine(reverseString.ReverseTheString("Pranav Kumar Verma"));
+            Console.WriteLine(reverseString.ReverseTheCharacter("Pranav Kumar Verma"));
             Console.ReadKey();
         }
     }
--- InterviewProblems/ReverseString.cs
+++ InterviewProblems/ReverseString.cs
@@ -4,15 +4,30 @@ namespace InterviewProblems
 {
     internal class ReverseString
     {
+        public string ReverseTheCharacter(string value)
+        {
+            string reversedCharacter = String.Empty;
+            if (value != null)
+            {
+                var splittedText = value.ToCharArray();
+                for (int i = splittedText.Length - 1; i >=0; i--)
+                {
+                    reversedCharacter += splittedText[i];
+                }
+            }
+
+            return reversedCharacter;
+        }
+
         public string ReverseTheString(string value)
         {
             string reversedString = String.Empty;
             if (value != null)
             {
-                var splittedText = value.ToCharArray();
-                for (int i = splittedText.Length - 1; i > - 1; i--)
+                var splitString = value.Split();
+                for (int i = splitString.Length-1; i >=0; i--)
                 {
-                    reversedString += splittedText[i];
+                    reversedString += splitString[i]+" ";
                 }
             }
 
