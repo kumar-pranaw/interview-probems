69e592da80abe4e4d934e7b86bd4ffee1904c871
Pranav kumar <sanupranav@outlook.com>
2022-03-29 07:57:40 +0530

    feat: Added new project for pattern printing and added simple pattern program

---
+++ PatternProblems/BasicPatternProblem.cs
@@ -0,0 +1,17 @@
+﻿namespace PatternProblems
+{
+    internal class BasicPatternProblem
+    {
+        public void PrintSimpleStarPattern(int n)
+        {
+            for (int row = 1; row <= n; row++)
+            {
+                for (int col = 1; col <= row; col++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
